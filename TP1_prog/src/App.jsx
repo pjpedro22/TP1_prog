eba4b65e623040d3510fc84ad0d0fd8ec7448510
Guillaume Legare <g23leg@gmail.com>
2023-06-23 15:10:17 -0400

    onMousseEnter & onMousseLeave + Abilities

--- TP1_prog/src/App.jsx
+++ TP1_prog/src/App.jsx
@@ -3,13 +3,6 @@ import Deck from './components/Deck'
 import './App.css'
 
 function App() {
-  // const ability = {
-  //   abilities: {
-  //     Static: 'Shock the target, with a potential to inflict paralysis. ',
-  //     Celebrate: 'The Pokémon can move again after defeating an enemy.',
-  //     Lightningrod: 'Absorbs all Electric-type moves to raise Attack.'
-  //   }
-  // }
 
   return (
     <div>
--- TP1_prog/src/components/Card.jsx
+++ TP1_prog/src/components/Card.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import './Card.css'
 
 const Card = (props) => {
+  const classnames = ['card'];
+
+  if (props.value === props.active) {
+    classnames.push('active');
+  }
+
+  const expandCard = () => {
+    props.onMouseEnter(props.value);
+  }
+
+  const collapseCard = () => {
+    props.onMouseLeave(props.value);
+  }
+
   return (
-    <div>
+    <div className={classnames.join(" ")} onMouseEnter={expandCard} onMouseLeave={collapseCard}>
       <h1>{props.name}</h1>
       <img src={props.img} alt={props.name} />
+      <div className="abilities">{props.children}</div>
     </div>
   )
 }
